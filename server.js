2aabc1410711ac87b735cb043e9438dedc036e5d
raunakshaw07 <raunakshaw1004@gmail.com>
2024-02-13 22:37:16 +0530

    Updated port

--- server.js
+++ server.js
@@ -20,4 +20,6 @@ require('./config/db')();
 // Routes
 app.use("/api", require('./routes/index'))
 
-app.listen(process.env.port, () => console.log(`Server running on port ${process.env.port}`))
+const PORT = process.env.port || 5000
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
